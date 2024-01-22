19e9bfffa4f5d958898f62ff066e76bb56f76dc0
luopei <luopei@holomatic.com>
2024-01-22 17:13:03 +0800

    Add: download to local_dir instead of cache

--- download_model.py
+++ download_model.py
@@ -1,4 +1,11 @@
 from huggingface_hub import snapshot_download
 from huggingface_hub import login
 
-snapshot_download(repo_id="meta-llama/Llama-2-7b-chat-hf", resume_download=True)
+# ref : https://huggingface.co/docs/huggingface_hub/guides/download
+
+# login, set enviroment variable HF_TOKEN
+login()
+
+local_dir = '.'
+local_dir_use_symlinks = False
+snapshot_download(repo_id="meta-llama/Llama-2-7b-chat-hf", resume_download=True, local_dir=local_dir, local_dir_use_symlinks=local_dir_use_symlinks)
